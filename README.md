4efd48c0428efa10e7e3d35b264c5ebb51079a56
Semyonic <semih.onay@bilgiedu.net>
2018-03-23 11:55:01 +0300

    Update project description and sample .env file

---
+++ README.md
@@ -0,0 +1,45 @@
+# semyonic-telegram-bot [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com) [![Codacy Badge](https://api.codacy.com/project/badge/Grade/d88e992469eb477bbf6521e8af5ad0d0)](https://www.codacy.com/app/semih.onay/telegram-playlist-bot?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=Semyonic/telegram-playlist-bot&amp;utm_campaign=Badge_Grade)
+
+It was a toy project to share playlist changes to my friends following playlists.
+
+> Detects changes on given users Spotify playlist then sends added tracks to the Telegram users via bot. (Spotify doesn't support such Events,WebHooks currently)
+
+## Pre-Installation
+
+1. Obtain ****Telegram Bot API Token**** from BotFather [@BotFather](https://telegram.me/BotFather)
+2. Create a Spotify Application from [Developer Spotify](https://developer.spotify.com/my-applications)
+
+## Installation
+
+```sh
+$ npm install
+```
+
+Then configure your ****.env**** file (Sample .env exists in this project)
+
+## Configuration
+
+You can change polling frequency in .env file
+
+### ****<center>.env Configuration Parameters</center>****
+
+| Name | Description |
+| --- | --- |
+| `TELEGRAM_BOT_TOKEN` | Your bot access token |
+| `TELEGRAM_ADMIN_ID` | Your Telegram ID (Stored in mongoDB after initializing chat with the bot) |
+| `SPOTIFY_ID` | Your Spotify Application ID|
+| `SPOTIFY_SECRET` | Your Spotify Application Secret|
+| `SPOTIFY_ID_ME` | Your Spotify Profile info (You can obtain via share profile button in Spotify App)|
+| `SPOTIFY_PLAYLIST_ID` | Your Spotify Playlist ID (You can obtain via share playlist button in Spotify App)|
+| `SPOTIFY_POLLING` | Polling frequency in MiliSeconds to check playlist changes (****Be aware of Rate Limits !****)|
+| `MONGO_URI` | Your mongoDB connection URL (You can use free 500mb from ****[mLab](https://mlab.com/)****)|
+
+## Usage
+
+```js
+npm start
+```
+
+## License
+
+MIT © [Semih Onay](https://semyonic.github.io)
--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-import _ from 'lodash';
-import bodyParser from 'body-parser';
-import express from 'express';
-import logger from 'morgan';
-import TeleBot from 'telebot';
-import mongoose from 'mongoose';
-import request from 'request';
+import _ from 'lodash'
+import bodyParser from 'body-parser'
+import express from 'express'
+import logger from 'morgan'
+import TeleBot from 'telebot'
+import mongoose from 'mongoose'
+import request from 'request'
 
-import User from './models/User';
-import Chat from './models/Chat';
-import PlayListSnap from './models/PlayListSnap';
+import User from './models/User'
+import Chat from './models/Chat'
+import PlayListSnap from './models/PlayListSnap'
 
-import index from './routes/index';
+import index from './routes/index'
 
-const app = express();
-const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
+const app = express()
+const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN)
 
 /**
  * Set Authorization info for Spotify
@@ -21,49 +21,49 @@ const bot = new TeleBot(process.env.TELEGRAM_BOT_TOKEN);
 const authOptions = {
   url: 'https://accounts.spotify.com/api/token',
   headers: {
-    Authorization: `Basic ${Buffer.from(`${process.env.SPOTIFY_ID}:${process.env.SPOTIFY_SECRET}`).toString('base64')}`,
+    Authorization: `Basic ${Buffer.from(`${process.env.SPOTIFY_ID}:${process.env.SPOTIFY_SECRET}`).toString('base64')}`
   },
   form: {
-    grant_type: 'client_credentials',
+    grant_type: 'client_credentials'
   },
-  json: true,
-};
+  json: true
+}
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 mongoose.connection.openUri(process.env.MONGO_URI).then(() => {
-  console.log('mongoDB Connected');
-  bot.start();
+  console.log('mongoDB Connected')
+  bot.start()
 }).catch((error) => {
-  console.log(error);
-});
+  console.log(error)
+})
 
 /**
  * Bot welcome message
  */
-bot.on(['/start'], (msg) => msg.reply.text('Hoş geldin !'));
+bot.on(['/start'], (msg) => msg.reply.text('Hoş geldin !'))
 
 /**
  * Send random tracks from playlist
  */
 bot.on('/rastgele', (msg) => {
-  msg.reply.text('Rastgele şarkı gelecek');
-});
+  msg.reply.text('Rastgele şarkı gelecek')
+})
 
 /**
  * Creator of the bot
  */
 bot.on('/yaratici', (msg) => {
-  msg.reply.text('@Semyonic');
-});
+  msg.reply.text('@Semyonic')
+})
 
 /**
  * Help command
  */
 bot.on('/yardim', (msg) => {
   bot.getChatMembersCount(msg.chat.id).then((resp) => {
-    msg.reply.text(`Bot ile konuşan kişi sayısı : ${resp} \n \nKullanılabilecek bot komutları \n \n/enson : Listeye eklenen en son parçayı getirir \n/rastgele : Listeden rastgele parça gönderir \n/yardim : Bu sayfayı gösterir \n/yaratici : Botun yazarı ile iletişime geçmek için`);
-  });
-});
+    msg.reply.text(`Bot ile konuşan kişi sayısı : ${resp} \n \nKullanılabilecek bot komutları \n \n/enson : Listeye eklenen en son parçayı getirir \n/rastgele : Listeden rastgele parça gönderir \n/yardim : Bu sayfayı gösterir \n/yaratici : Botun yazarı ile iletişime geçmek için`)
+  })
+})
 
 /**
  * Reply every user input
@@ -71,28 +71,28 @@ bot.on('/yardim', (msg) => {
 bot.on('text', (msg) => {
   if (!msg.hasOwnProperty('entities')) {
     User.findOne({$where: 'userId ==' + msg.from.id}, (error) => {
-      if (error) return error;
+      if (error) return error
       User.create({
         userName: msg.from.first_name,
-        userId: msg.from.id,
+        userId: msg.from.id
       }, (err) => {
-        if (err) return err;
-      });
-    });
+        if (err) return err
+      })
+    })
     Chat.create({
       chatId: msg.chat.id,
       userName: msg.from.first_name,
-      message: msg.text,
+      message: msg.text
     }, (err, post) => {
-      if (err) return err;
-    });
+      if (err) return err
+    })
     if (msg.text.includes('kimsin')) {
-      msg.reply.text('@Semyonic tarafından yaratılmış bir botum.');
+      msg.reply.text('@Semyonic tarafından yaratılmış bir botum.')
     } else {
-      msg.reply.text(msg.text);
+      msg.reply.text(msg.text)
     }
   }
-});
+})
 
 /**
  * Polling playlist snapshot_id'to detect changes on playlist and
@@ -101,37 +101,36 @@ bot.on('text', (msg) => {
 setInterval(() => {
   request.post(authOptions, (error, response, body) => {
     if (!error && response.statusCode === 200) {
-
-      const token = body.access_token;
+      const token = body.access_token
       const options = {
         url: `https://api.spotify.com/v1/users/${process.env.SPOTIFY_ID_ME}/playlists/${process.env.SPOTIFY_PLAYLIST_ID}?fields=(snapshot_id)`,
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${token}`
         },
-        json: true,
-      };
+        json: true
+      }
 
       request.get(options, (err, resp) => {
-        if (err) return err;
+        if (err) return err
 
         PlayListSnap.findOne({snapshot_id: resp.body.snapshot_id}, (error, result) => {
-          if (error) return error;
+          if (error) return error
           if (result === null) {
             PlayListSnap.create({
-              snapshot_id: resp.body.snapshot_id,
+              snapshot_id: resp.body.snapshot_id
             }, (err) => {
-              if (err) return err;
-            });
-            getTracks();
+              if (err) return err
+            })
+            getTracks()
           }
-        });
-      });
+        })
+      })
     } else if (response.statusCode === 429) {
-      bot.sendMessage(process.env.TELEGRAM_ADMIN_ID, 'Spotify Rate Limit Triggered ! Shuting down');
-      process.exit(1);
+      bot.sendMessage(process.env.TELEGRAM_ADMIN_ID, 'Spotify Rate Limit Triggered ! Shuting down')
+      process.exit(1)
     }
-  });
-}, process.env.SPOTIFY_POLLING);
+  })
+}, process.env.SPOTIFY_POLLING)
 
 /**
  * Gets,sorts and sends tracks to Telegram users
@@ -139,39 +138,38 @@ setInterval(() => {
 const getTracks = () => {
   request.post(authOptions, (error, response, body) => {
     if (!error && response.statusCode === 200) {
-
-      const token = body.access_token;
+      const token = body.access_token
       const options = {
         url: `https://api.spotify.com/v1/users/11172021434/playlists/2jzzDTeKB60YgehWhayJVq/tracks?fields=items(added_at,track(external_urls(spotify)),limit,next,offset,total)`,
         headers: {
-          Authorization: `Bearer ${token}`,
+          Authorization: `Bearer ${token}`
         },
-        json: true,
-      };
+        json: true
+      }
 
       request.get(options, (err, resp) => {
-        if (err) return err;
-        let sorted = _.sortBy(resp.body.items, ['added_at']);
-        let ordered = _.orderBy(sorted, ['added_at'], ['asc']);
-        let track = JSON.stringify(ordered[ordered.length - 1].track.external_urls.spotify);
+        if (err) return err
+        let sorted = _.sortBy(resp.body.items, ['added_at'])
+        let ordered = _.orderBy(sorted, ['added_at'], ['asc'])
+        let track = JSON.stringify(ordered[ordered.length - 1].track.external_urls.spotify)
         User.find({}, {userId: 1, _id: 0}, (err, users) => {
           users.forEach((user) => {
-            bot.sendMessage(user.userId, 'Listeye yeni parça eklendi ! \n \n' + track);
-          });
-        });
-      });
+            bot.sendMessage(user.userId, 'Listeye yeni parça eklendi ! \n \n' + track)
+          })
+        })
+      })
     }
-  });
-};
+  })
+}
 
-app.use(logger('dev'));
+app.use(logger('dev'))
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
-  extended: false,
-}));
+  extended: false
+}))
 
-app.use('/', index);
+app.use('/', index)
 
 /**
  * Triggered from Spotify for login operations
@@ -181,31 +179,31 @@ app.get('/callback', (req, res) => {
    * TODO
    * Login to the chat-app
    */
-});
+})
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
 /* eslint no-unused-vars: 0 */
 app.use((err, req, res) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
   // render the error page
-  res.status(err.status || 500);
-  res.json(err);
-});
+  res.status(err.status || 500)
+  res.json(err)
+})
 
 // Handle uncaughtException
 process.on('uncaughtException', (err) => {
   // Send errors to Telegram Admin then crash
-  bot.sendMessage(process.env.TELEGRAM_ADMIN_ID, JSON.stringify(err));
-  process.exit(1);
-});
+  bot.sendMessage(process.env.TELEGRAM_ADMIN_ID, JSON.stringify(err))
+  process.exit(1)
+})
 
-export default app;
+export default app
